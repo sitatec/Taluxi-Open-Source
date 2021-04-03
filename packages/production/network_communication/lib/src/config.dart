4eaddaa10d5600c6221d2d77bf64d013e28dd019
Sita Bérété <sita.berete.3@gmail.com>
2021-04-03 19:32:20 -0400

    Update config.dart

--- packages/production/network_communication/lib/src/config.dart
+++ packages/production/network_communication/lib/src/config.dart
@@ -1,2 +1,2 @@
-const oneSignalAppId = '927b1a02-798c-4ebb-92b5-56b34ec055a1';
-const agoraAppId = '1cf08144cb2a438bb2b9dfcd7cdc86c5';
+const oneSignalAppId = 'YOUR-APP-ID-HERE';
+const agoraAppId = 'YOUR-APP-ID-HERE';
